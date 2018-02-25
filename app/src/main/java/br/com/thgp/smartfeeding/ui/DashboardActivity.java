77f6cec18bb9c0109804583eba190be3757fae49
Tiago Pacheco <tiagoh.gpacheco@gmail.com>
2018-02-25 13:30:07 -0300

    all necessary activities

---
+++ app/src/main/java/br/com/thgp/smartfeeding/ui/DashboardActivity.java
@@ -0,0 +1,15 @@
+package br.com.thgp.smartfeeding.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.thgp.smartfeeding.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
